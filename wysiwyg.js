a75d8d2a337311460742c495ce5b3349ce157235
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-01-18 13:22:15 -0600

    needs event listeners

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,40 +1,53 @@
-{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-  lifespan: {
-    birth: 1747,
-    death: 1797
-  }
-},
-{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-  lifespan: {
-    birth: 1747,
-    death: 1797
-  }
-},
-{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-  lifespan: {
-    birth: 1747,
-    death: 1797
-  }
-},
-{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-  lifespan: {
-    birth: 1747,
-    death: 1797
-  }
+var people = [
+	{title: "Samurai",
+	  name: "Tomoe Gozen",
+	  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+	  lifespan: {
+	    birth: 1747,
+	    death: 1797
+	  }
+	},
+	{title: "Samurai",
+	  name: "Tomoe Gozen",
+	  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+	  lifespan: {
+	    birth: 1747,
+	    death: 1797
+	  }
+	},
+	{title: "Samurai",
+	  name: "Tomoe Gozen",
+	  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+	  lifespan: {
+	    birth: 1747,
+	    death: 1797
+	  }
+	},
+	{title: "Samurai",
+	  name: "Tomoe Gozen",
+	  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+	  lifespan: {
+	    birth: 1747,
+	    death: 1797
+	  }
+	}
+]
+
+
+for (var i = 0; i < people.length; i++) {
+	var title = people[i].title
+	var name = people[i].name
+	var bio = people[i].bio
+	var birth = people[i].lifespan.birth
+	var death = people[i].lifespan.death
+
+	var card = "<person class='card'><header class='card-header'>"+title+" - "+name+"</header><section class='card-bio'>"+bio+"</section><footer class='card-lifespan'>"+birth+"-"+death+"</footer></person>"
+
+	var cardSection = document.getElementById("card-section")
+	cardSection.innerHTML += card
+
 }
